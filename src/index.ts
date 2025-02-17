5a6e5d0737166ebe510af700c7962f408a181793
Sarthak Naik <sarthaknaik@MB-snai.local>
2025-02-17 12:51:31 +0530

    dot added

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const limiter = rateLimit({
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below
+  // store: ... , // Redis, Memcached, etc. See below.
 });
 
 // Middleware
